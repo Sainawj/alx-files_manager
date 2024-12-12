6c083ea613fbb895a75064b3013b214a5de99e9d
Jonathan Saina <51122713+Sainawj@users.noreply.github.com>
2024-12-13 00:37:11 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,19 @@
 import express from 'express';
 import AppController from '../controllers/AppController.js';
 import UsersController from '../controllers/UsersController.js';
+import AuthController from '../controllers/AuthController.js';
 
 const router = express.Router();
 
 // Existing endpoints
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
-
-// New endpoint for creating users
 router.post('/users', UsersController.postNew);
+
+// New authentication endpoints
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
 
 export default router;
 
