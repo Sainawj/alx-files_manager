7a41fa26c4ac8af2e524682bf0f52f8cc21ad13d
Jonathan Saina <51122713+Sainawj@users.noreply.github.com>
2024-12-13 00:33:31 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import routes from './routes/index.js';
+import bodyParser from 'body-parser';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// Middleware to parse JSON bodies
+app.use(bodyParser.json());
 
 // Load all routes
 app.use('/', routes);
